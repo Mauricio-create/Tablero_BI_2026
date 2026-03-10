41103ee044c9c06acbfe3b1c09ac55e2e44029b6
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-09 20:52:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # Sección de importación de módulos
 from Modules.UI.header import show_header
 from Modules.ecobici_service import EcobiciLoader
+import streamlit as st
+import pandas as pd
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
+
+ecobici = EcobiciService()
+df = eco.merge_data()
+st.write(df)
