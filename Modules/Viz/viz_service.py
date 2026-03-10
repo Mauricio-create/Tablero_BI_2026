c514a72a2a876fffabc4ef04bd6315c781af9a55
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-09 21:07:02 -0600

    Update viz_service.py

--- Modules/Viz/viz_service.py
+++ Modules/Viz/viz_service.py
@@ -1,1 +1,38 @@
+import plotly.express as px
 
+
+class EcobiciMap:
+
+    def __init__(self, df):
+        """
+        Constructor de la clase
+        df: DataFrame con información de estaciones
+        """
+        self.df = df
+
+    def create_map(self):
+        """
+        Genera el mapa de estaciones Ecobici en CDMX
+        """
+
+        fig = px.scatter_mapbox(
+            self.df,
+            lat="lat",
+            lon="lon",
+            hover_name="name",
+            hover_data={
+                "capacity": True,
+                "num_bikes_available": True,
+                "num_docks_available": True
+            },
+            zoom=11,
+            height=600
+        )
+
+        fig.update_layout(
+            mapbox_style="open-street-map",
+            title="Estaciones Ecobici - Ciudad de México",
+            margin={"r":0,"t":40,"l":0,"b":0}
+        )
+
+        return fig
