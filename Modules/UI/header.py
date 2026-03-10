9b5ca2120c8dbe252c4c873f5ac62315956a6031
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-09 21:48:53 -0600

    Update header.py

--- Modules/UI/header.py
+++ Modules/UI/header.py
@@ -10,4 +10,4 @@ def show_header(text_title: str):
     with col2:
         st.title(text_title)
         st.caption("📘 Developed for: *Business Intelligence (Graduate Level)*")
-        st.caption("Instructor: Mauricio González Castelazo (2026), Universidad Panamericana")
+        st.caption("Alumno: Mauricio González Castelazo (2026), Universidad Panamericana")
