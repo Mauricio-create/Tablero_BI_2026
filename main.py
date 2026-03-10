b5f6e94557574de5320f8477ca406b6ec7952553
Mauricio Castelazo <77642083+Mauricio-create@users.noreply.github.com>
2026-03-09 20:49:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Sección de importación de módulos
 from Modules.UI.header import show_header
+from Modules.ecobici_service import EcobiciLoader
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
